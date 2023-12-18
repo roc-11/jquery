254fbce50ac852492ba6394cb70ce80f9cb2018e
roc-11 <>
2023-12-18 09:50:11 +0000

    Use jQuery to target css properties and change them

--- js/table-script.js
+++ js/table-script.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
 
+    /*********  Example - change css ************/
+    // $("h2").css("text-decoration", "underline"); // underlines all <h2> elements
+    $("h2").addClass("underline");
+    // $("ul").css("border", "solid 1px #ccc");
+    $("ul").removeClass("border");
+
+    $("tr:odd").addClass("odd");
+    $("tr:even").addClass("even");
 });
