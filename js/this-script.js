b5be7e8bb3d97e464244512c53cbaa084a1a7e03
roc-11 <>
2023-12-18 12:43:50 +0000

    Fix error in JS file

--- js/this-script.js
+++ js/this-script.js
@@ -13,6 +13,6 @@ $('button').mouseleave(function () {
 $(document).ready(function () {
     $('.box').on("click", function () {
         var classNames = $(this).attr("class").split(" ");
-        $("." + classNames[1]).css("background-color", "red";);
-})
-})
+        $("." + classNames[1]).css("background-color", "red");
+    });
+});
